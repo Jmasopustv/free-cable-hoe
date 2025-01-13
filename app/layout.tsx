a0cce02199716f2ce77ed0f43368ae00ae716af3
John Masopust V <jjmasopustv@gmail.com>
2025-01-13 15:10:25 -0700

    collection fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,35 +9,51 @@ import { Toaster } from 'sonner';
 import './globals.css';
 
 const { TWITTER_CREATOR, TWITTER_SITE, SITE_NAME } = process.env;
+
+// Set the base URL for metadata
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : 'http://localhost:3000';
-const twitterCreator = TWITTER_CREATOR ? ensureStartsWith(TWITTER_CREATOR, '@') : undefined;
-const twitterSite = TWITTER_SITE ? ensureStartsWith(TWITTER_SITE, 'https://') : undefined;
 
+const twitterCreator = TWITTER_CREATOR
+  ? ensureStartsWith(TWITTER_CREATOR, '@')
+  : undefined;
+
+const twitterSite = TWITTER_SITE
+  ? ensureStartsWith(TWITTER_SITE, 'https://')
+  : undefined;
+
+// Metadata configuration for the layout
 export const metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: SITE_NAME!,
-    template: `%s | ${SITE_NAME}`
+    default: SITE_NAME || 'Free Cable',
+    template: `%s | ${SITE_NAME || 'Free Cable'}`,
   },
   robots: {
     follow: true,
-    index: true
+    index: true,
   },
   ...(twitterCreator &&
     twitterSite && {
       twitter: {
         card: 'summary_large_image',
         creator: twitterCreator,
-        site: twitterSite
-      }
-    })
+        site: twitterSite,
+      },
+    }),
 };
 
-export default async function RootLayout({ children }: { children: ReactNode }) {
+// Root layout component
+export default async function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  // Retrieve cart ID from cookies
   const cartId = (await cookies()).get('cartId')?.value;
-  // Don't await the fetch, pass the Promise to the context provider
+
+  // Fetch cart data without awaiting, passing Promise to CartProvider
   const cart = getCart(cartId);
 
   return (
@@ -45,11 +61,8 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
         <CartProvider cartPromise={cart}>
           <Navbar />
-          <main>
-            {children}
-            <Toaster closeButton />
-            <WelcomeToast />
-          </main>
+          <main className="min-h-screen">{children}</main>
+          <Toaster />
         </CartProvider>
       </body>
     </html>
